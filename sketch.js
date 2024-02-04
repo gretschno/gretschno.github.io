b7025d316241b8b06be2490dfd556921f4716fa3
gretschno <whatacolourmusic@gmail.com>
2024-02-04 00:26:56 -0500

    added music

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
 var angle = 0;
+var song; // Declare an audio file variable
+
+function preload() {
+  song = loadSound('gretschno_site_loop.aif');
+}
 
 function setup() {
   createCanvas(600, 600);
+  song.loop(); // Start looping the audio
 }
 
 function draw() {
@@ -11,7 +17,7 @@ function draw() {
   line(width / 2, height, width / 2, height - 100);
   translate(width / 2, (height / 4) * 3.5);
 
-  angle = map(mouseX, 0, width, 0, TWO_PI); // Use mouseX position to set the angle
+  angle = map(mouseX, 0, width, 0, TWO_PI);
   branch(110);
 }
 
@@ -25,5 +31,13 @@ function branch(len) {
     pop();
     rotate(-angle);
     branch(len * 0.7);
+  }
+}
+
+function mousePressed() {
+  if (song.isPlaying()) {
+    song.pause(); // Pause the audio if it's already playing
+  } else {
+    song.play(); // Play the audio if it's paused
   }
 }
